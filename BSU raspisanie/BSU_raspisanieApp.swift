5d21291c13cf6a6b4cd2c009ab29c7e2d73987f9
me <pashatimofeeff@gmail.com>
2023-09-26 21:35:56 +0300

    Initial Commit

---
+++ BSU raspisanie/BSU_raspisanieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BSU_raspisanieApp.swift
+//  BSU raspisanie
+//
+//  Created by 123 on 26.09.23.
+//
+
+import SwiftUI
+
+@main
+struct BSU_raspisanieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BSU raspisanie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BSU raspisanie
+//
+//  Created by 123 on 26.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
